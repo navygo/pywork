dd62344d70f94c90dfb4c54834783b4904c3217e
navygo <whj001@126.com>
2018-05-14 21:24:31 +0800

    sing

---
+++ singleton/CirclArea.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+
+print("Enter circl r :")
+r = input()
+r = float(r)
+s = math.pi*r*r
+print("Circl r:{:6.4f} Area is:{:6.4f}".format(r,s))
---
+++ singleton/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+number=int(input("Enter an integer"))
+if number<=100:
+    print("Your number is smaller than equal to 100")
+else:
+    print("Your number is greater than 100")
