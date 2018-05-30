6e3e18f3525c1f7eca6728302b983c2698924dd1
navygo <whj001@126.com>
2018-05-30 22:38:58 +0800

    2018-05-30 update 2

---
+++ django/my_blog/article/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+
+def home(request):
+    return HttpResponse("Hello World,Django")
+
+def detail(request,my_args):
+    post =Article.objects.all()[int(my_args)]
+    str = ("title=%s,category=%s,date_time=%s,content=%s" %(post.title,post.category,post.date_time,post.content))
+    return HttpResponse(str)
+
+   # return HttpResponse("You're looking at my_args %s." % my_args)
+
+
+# Create your views here.
